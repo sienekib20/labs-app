406f136f20d6b8e7b19e08d16bbc58d4e152a63a
sienekib20 <sienekib@gmail.com>
2024-04-12 18:25:41 +0100

    updates

--- resources/views/partials/leitor/bottom-leitor.view.php
+++ resources/views/partials/leitor/bottom-leitor.view.php
@@ -1,37 +1,45 @@
-<div class="player-in-bottom">
+<div class="inbottom-player">
     <div class="container">
-        <div class="cover">
-            <img src="<?= asset('img/bg-2.jpg') ?>" alt="">
-        </div>
-        <div class="player-controls">
-            <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path d="M12 12l12-7v14l-12-7zm-12 0l12-7v14l-12-7z" />
-                </svg>
-            </a>
-            <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path d="M3 22v-20l18 10-18 10z" />
-                </svg>
-            </a>
-            <a href="">
-                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
-                    <path d="M10 24h-6v-24h6v24zm10 0h-6v-24h6v24zm-11-23h-4v22h4v-22zm10 0h-4v22h4v-22z" />
-                </svg>
-            </a>
-            <a href="">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path d="M0 19v-14l12 7-12 7zm12 0v-14l12 7-12 7z" />
-                </svg>
-            </a>
-        </div>
-        <div id="timeline"></div>
-        <div class="player-close">
-            &times;
+        <div class="row">
+            <div class="col-3">
+                <span class="card-heading text-white d-block" style="margin-bottom: -5px;">Tudo que eu Quero</span>
+                <small class="text-muted">Album albumname | <a href="<?= route('playlist', 1) ?>">Artista name</a></small>
+            </div>
+            <div class="col-2">
+                <div class="player-controls">
+                    <a href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M12 12l12-7v14l-12-7zm-12 0l12-7v14l-12-7z" />
+                        </svg>
+                    </a>
+                    <a href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M3 22v-20l18 10-18 10z" />
+                        </svg>
+                    </a>
+                    <a href="" class="d-none">
+                        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
+                            <path d="M10 24h-6v-24h6v24zm10 0h-6v-24h6v24zm-11-23h-4v22h4v-22zm10 0h-4v22h4v-22z" />
+                        </svg>
+                    </a>
+                    <a href="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M0 19v-14l12 7-12 7zm12 0v-14l12 7-12 7z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 d-none d-lg-block">
+                <div id="timeline"></div>
+            </div>
+            <div class="col-1 ml-auto">
+                <div class="player-close">
+                    &times;
+                </div>
+            </div>
         </div>
     </div>
 </div>
-
 <input type="hidden" id="load_me" value="<?= media('tudo') ?>">
 
 <script>
